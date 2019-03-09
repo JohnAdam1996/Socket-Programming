8202eba695ad13540ad8e244665782292e450b79
John Adam <john.adam@stonybrook.edu>
2019-03-09 15:53:58 -0500

    Update client(1).py

--- client(1).py
+++ client(1).py
@@ -2,9 +2,10 @@ import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-port = 2000                                 # Choose the port number your server is bound to
+port = 12345                                 # Choose the port number your server is bound to
+host = '127.0.0.1'
 
-sock.connect(('localhost', port))           # Connects to given port number on the local machine
+sock.connect((host, port))           # Connects to given port number on the local machine
 
 while True:
     cmd = input("")                         # Get command line input to send to server
