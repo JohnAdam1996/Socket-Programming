34be27fec7ac02136732947d036d1fd3eb2d188c
John Adam <john.adam@stonybrook.edu>
2019-03-10 11:55:29 -0400

    Update Skeleton.py

--- Skeleton.py
+++ Skeleton.py
@@ -55,7 +55,7 @@ def main():
     operations = {'get': get, 'put': put, 'delete': delete, 'clear': clear, 'quit': quit}
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)         # Create a socket object
     host = '127.0.0.1' # Get local machine name
-    port = 12345                # Reserve a port for your service.
+    port = 2000                # Reserve a port for your service.
     s.bind((host, port))        # Bind to the port
 
     s.listen(5)                 # Now wait for client connection.
@@ -63,11 +63,10 @@ def main():
         c, addr = s.accept()     # Establish connection with client.
         print ('Got connection from', addr)
         while True:
-            print(operations[get])
             modifiedConnected = c.recv(1024)
             message = modifiedConnected.decode()
             print(message)
-            if(message == operations['get']):
+            if(operations['get']):
                 get()
             elif(message == operations['put']):
                 put()
