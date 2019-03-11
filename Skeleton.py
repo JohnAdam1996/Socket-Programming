23216cc347ca040b37a9d75f17baa5c60f56430a
John Adam <john.adam@stonybrook.edu>
2019-03-10 23:27:23 -0400

    Update Skeleton.py

--- Skeleton.py
+++ Skeleton.py
@@ -1,7 +1,7 @@
 import socket
 
 dictionary = {}                                         # Dictionary that will hold the key-value pairs
-
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # HTTP return messages
 HTTP = 'HTTP/1.1 '
@@ -13,31 +13,29 @@ print("I am here")
 
 # Implement the get function that returns the value for the specific key from the dictionary
 def get(x):
-    if(x == dictonary[x]):
-        return print(OK)
-    elif(x != dictonary[x]):
-        return print(NOT_FOUND)
+    print("I am in get")
+    if(x == dictionary.get(x)):
+        print(OK)
+    elif(x != dictionary):
+        print(NOT_FOUND)
     
 
 
 # Implement the put function that updates the value for the specific key in the dictionary
-def put(x):
-    dictionary = [x]
-    return
+def put(x, y):
+    dictionary[x] = y
+    print(OK)
 
 
 # Implement the delete function that will remove the key-value pair from the dictionary
 def delete(x):
-    if(x == dictonary[x]):
-        dictonary.pop[x]
-        return print(OK,"\nRemoved ", x)
-    elif(x != dictonary[x]):
-        return print(NOT_FOUND)
+    dictionary.pop[x]
+    print(OK)
 
 # Implement the delete function that will remove all the key-value pairs from the dictionary
 def clear():
     dictionary.clear()
-    return print(OK,"\nRemoved ", x)
+    print(OK)
 
 
 # Implement the quit function that will end the connection with the client
@@ -52,8 +50,7 @@ def quit():
 # it will listen to local connection requests.
 def main():
 	# You can use this operations dictionary to get the corresponding function: For example $func = operations['get'](params) is equal to $get(params) 
-    operations = {'get': get, 'put': put, 'delete': delete, 'clear': clear, 'quit': quit}
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)         # Create a socket object
+             # Create a socket object
     host = '127.0.0.1' # Get local machine name
     port = 2000                # Reserve a port for your service.
     s.bind((host, port))        # Bind to the port
@@ -63,21 +60,45 @@ def main():
         c, addr = s.accept()     # Establish connection with client.
         print ('Got connection from', addr)
         while True:
+            print("Why am I here")
             modifiedConnected = c.recv(1024)
             message = modifiedConnected.decode()
             print(message)
-            if(operations['get']):
-                get()
-            elif(message == operations['put']):
-                put()
-            elif(message == operations['delete']):
-                delete()
-            elif(message == operations['clear']):
+            split = message.split(' ')
+            print(split)
+            if(split[0].lower() == 'get'):
+                if(len(split) > 2):
+                    print(BAD_REQUEST)
+                    break;
+                get(split[1])
+                continue;
+            elif(split[0].lower() == 'put'):
+                if(len(split) > 3):
+                    print(BAD_REQUEST)
+                    break;
+                x = split[1]
+                y = split[2]
+                put(x, y)
+                continue;
+            elif(split[0].lower() == 'delete'):
+                if(len(split) > 2):
+                    print(BAD_REQUEST)
+                    break;
+                delete(split[1])
+            elif(split[0].lower() == 'clear'):
+                if(len(split) > 1):
+                    print(BAD_REQUEST)
+                    break;
                 clear()
-            elif(message == operations['quit']):
+            elif(split[0].lower() == 'quit'):
+                if(len(split) > 1):
+                    print(BAD_REQUEST)
+                    break;
                 quit()
             else:
-                print(BAD_REQUEST)
+                print(UNSUPPORTED)
+                continue;
+        
 
 if __name__ == '__main__':
-      main()
+    main()
