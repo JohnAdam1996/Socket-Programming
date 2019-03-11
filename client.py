0547ef8d51f0f958cb2454c8ee69c4fc7baab3b9
John Adam <john.adam@stonybrook.edu>
2019-03-10 23:27:30 -0400

    Update client.py

--- client.py
+++ client.py
@@ -1,16 +1,26 @@
-# Import socket module 
-import socket			 
 
-# Create a socket object 
-s = socket.socket()		 
+import socket
 
-# Define the port on which you want to connect 
-port = 12345				
 
-# connect to the server on local computer 
-s.connect(('127.0.0.1', port)) 
+def client_program():
+    host = socket.gethostname()  # as both code is running on same pc
+    port = 5000  # socket server port number
 
-# receive data from the server 
-print (s.recv(1024)) 
-# close the connection 
-s.close()	 
+    client_socket = socket.socket()  # instantiate
+    client_socket.connect((host, port))  # connect to the server
+
+    message = input(" -> ")  # take input
+
+    while message.lower().strip() != 'bye':
+        client_socket.send(message.encode())  # send message
+        data = client_socket.recv(1024).decode()  # receive response
+
+        print('Received from server: ' + data)  # show in terminal
+
+        message = input(" -> ")  # again take input
+
+    client_socket.close()  # close the connection
+
+
+if __name__ == '__main__':
+    client_program()
